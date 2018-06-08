59aaeb6af7241bc604ab18258d6bbb6763cca414
Shingo Tomioka <s.tomioka.103@gmail.com>
2018-06-08 17:49:35 +0900

    Use sub_test_case

--- test/join_test.rb
+++ test/join_test.rb
@@ -3,10 +3,15 @@ require 'test/unit'
 require_relative '../src/index'
 
 class JoinTest < Test::Unit::TestCase
-  def test_pick_cell
-    filled_matrix = "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
-    assert_equal nil, pick_cell(filled_matrix)
-    unfilled_matrix = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
-    assert_equal [8, 9], pick_cell(unfilled_matrix)
+  sub_test_case "pick_cell" do
+    test "with_filled_matrix" do
+      matrix = "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
+      assert_equal nil, pick_cell(matrix)
+    end
+
+    test "with_unfilled_matrix" do
+      matrix = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+      assert_equal [7, 9], pick_cell(matrix)
+    end
   end
 end
